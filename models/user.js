6468955fd37ef2c7c35dcdc4cc048f44aee7f490
knh369 <knh369@konyadh.gov.tr>
2018-12-24 16:05:34 +0300

    daily edu

--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// bcryptjs zamanı
+var bcryptjs = require('bcryptjs'); // bcrypt sürüm problemleri nedeniyle olmadı ve vakit kaybetmeden bcryptjs e geçiş yaptık
+
 // Çizelge bölümü
 var kullanıcıŞeması = new Schema({
     email: {
@@ -25,6 +28,18 @@ var kullanıcıŞeması = new Schema({
         required: true
     }
 });
+
+// Veritabanına kaydetmeden önce şifreyi "hash" le
+kullanıcıŞeması.pre('save', function (next) { // Öncesinde (pre) kaydet (save) komutuyla kullanıyoruz. Burada kritik nokta:  " (next) => " şeklinde yazamadık, hata veriyor (!)
+    var kullanıcı = this; // "this", kullanıcı nın formda girdiği bilgiler ile oluşan nesneyi referans gösterir. "kullanıcı" nesnesini ve bilgilerini temsil eder
+    bcryptjs.hash(kullanıcı.password, 10, (err, hash) => { // "kullanıcı", yukarıdakiyle aynı. Sayı arttıkça güvenlik artar, sistem yavaşlar. 
+        if (err) {
+            return next(err);
+        }
+        kullanıcı.password = hash; // kullanıcı şifresini, "hash" lanmışıyla değiştir
+        next();
+    })
+})
 
 var Kullanıcı = mongoose.model('Kullanıcılar', kullanıcıŞeması);
 module.exports = Kullanıcı;
